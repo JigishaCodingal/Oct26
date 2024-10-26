0bb9a58016d5bf9579fa44a2afc9b3f7c5b1c2bf
jigishacodingal <jigishacodingal@gmail.com>
2024-10-26 15:55:05 +0530

    hi

---
+++ a.py
@@ -0,0 +1,33 @@
+class radius():
+
+def__init__(self)
+
+self.r=3
+
+def print_area(self):
+
+print("Result is :", self.r*self.r*3.14)
+
+
+str1 = area()
+
+str1.get_area()
+
+str1.print_area()
+
+class radius():
+
+def__init__(self)
+
+self.r=3
+
+def print_perimeter(self):
+
+print("Result is :", self.p*self.p*3.14)
+
+
+str1 = perimeter()
+
+str1.get_perimeter()
+
+str1.print_perimeter()
